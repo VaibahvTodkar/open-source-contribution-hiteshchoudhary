c0c66ee640bda8c770204771bf2cb87516a3cd3d
Vaibhav Todkar <78915720+VaibahvTodkar@users.noreply.github.com>
2025-05-21 11:49:31 +0530

    Add some java Sample Program

--- Java Programs/StreamFilterExample.java
+++ Java Programs/StreamFilterExample.java
@@ -4,8 +4,8 @@ import java.util.stream.*;
 public class StreamFilterExample {
     public static void main(String[] args) {
         
-        List<String> names = Arrays.asList("Vaibhav", "Vivek", "Ajay", "Ankit", "Vinay");
         // filter an names list that starts with V
+        List<String> names = Arrays.asList("Vaibhav", "Vivek", "Ajay", "Ankit", "Vinay");
        
         List<String> filteredNames = names.stream()               
                                           .filter(name -> name.startsWith("V")) 
